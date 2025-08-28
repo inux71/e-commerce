09371c29fac1a51a27098e6bd235d0c767e8bf9c
Kacper Grabiec <grabiec.kacper01@gmail.com>
2025-08-28 14:40:28 +0200

    [server] add project files

---
+++ server/e-commerce/src/test/java/com/grabieckacper/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.grabieckacper.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
