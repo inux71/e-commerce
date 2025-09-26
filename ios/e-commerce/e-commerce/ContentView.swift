b516ed30afb4bfd9696ab6ac5e2ed974447e722e
Kacper Grabiec <grabiec.kacper01@gmail.com>
2025-09-26 12:37:35 +0200

    [ios] add project files

---
+++ ios/e-commerce/e-commerce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  e-commerce
+//
+//  Created by Kacper Grabiec on 26/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
